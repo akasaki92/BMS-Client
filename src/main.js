b4adbd35adfdef9341f4c2df21947819716833f9
akasaki92 <adam.wikrama@gmail.com>
2021-05-02 11:23:50 +0700

    add axios, vuetify, router, vuex

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import "./registerServiceWorker";
+import VueMobileDetection from "vue-mobile-detection";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueMobileDetection);
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	store,
+	vuetify,
+	render: (h) => h(App),
+}).$mount("#app");
