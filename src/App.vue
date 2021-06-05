32c6dc90e5c1827b446e9524a297946b91d92039
akasaki92 <adam.wikrama@gmail.com>
2021-06-05 07:25:00 +0700

    fix app layout

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,33 @@
 <template>
-	<v-app>
-		<div v-if="$isMobile()">
-			<v-app-bar app color="primary" dark flat v-if="isHome">
-				<v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-				<v-toolbar-title to="/dashboard">{{ appName }}</v-toolbar-title>
-				<v-spacer></v-spacer>
-				<v-btn icon>
-					<v-btn icon dark @click="setDialogComponent('notification')">
-						<v-icon>mdi-bell</v-icon>
-					</v-btn>
+	<v-app v-if="$isMobile()">
+		<v-app-bar app color="primary" dark flat v-if="isHome">
+			<v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+			<v-toolbar-title to="/dashboard">{{ appName }}</v-toolbar-title>
+			<v-spacer></v-spacer>
+			<v-btn icon>
+				<v-btn icon dark @click="setDialogComponent('notification')">
+					<v-icon>mdi-bell</v-icon>
 				</v-btn>
-			</v-app-bar>
-			<v-main>
-				<v-container fluid>
-					<v-slide-y-transition>
-						<router-view />
-					</v-slide-y-transition>
-				</v-container>
-			</v-main>
-			<v-card>
-				<v-footer absolute app>
-					<v-card-text class="text-center">
-						&copy; {{ new Date().getFullYear() }} -
-						<strong>Hikiko Teams</strong>
-					</v-card-text>
-				</v-footer>
-			</v-card>
-		</div>
-		<div v-else>Please access on Mobile/Smartphone Only</div>
+			</v-btn>
+		</v-app-bar>
+		<v-main>
+			<v-container fluid>
+				<v-slide-y-transition>
+					<router-view />
+				</v-slide-y-transition>
+			</v-container>
+		</v-main>
+		<v-card>
+			<v-footer absolute app>
+				<v-card-text class="text-center">
+					&copy; {{ new Date().getFullYear() }} -
+					<strong>Hikiko Teams</strong>
+				</v-card-text>
+			</v-footer>
+		</v-card>
+	</v-app>
+	<v-app v-else>
+		Please open in smartphone
 	</v-app>
 </template>
 
